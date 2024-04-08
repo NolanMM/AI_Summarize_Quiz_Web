4262a1e98d30bd9404a7a6913236ab97e2113a59
NolanM <minhlenguyen7@gmail.com>
2024-04-08 14:24:45 -0400

    Add Login and Sign Up

---
+++ AI_Summarize_Quiz_Web/Models/EnterpriseProjectContext.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace AI_Summarize_Quiz_Web.Models
+{
+    public class EnterpriseProjectContext : IdentityDbContext<User>
+    {
+        public EnterpriseProjectContext(DbContextOptions<EnterpriseProjectContext> options)
+        : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public static async Task CreateAdminUser(IServiceProvider serviceProvider)
+        {
+            UserManager<User> userManager =
+                serviceProvider.GetRequiredService<UserManager<User>>();
+            RoleManager<IdentityRole> roleManager = serviceProvider
+                .GetRequiredService<RoleManager<IdentityRole>>();
+
+            string username = "admin";
+            string password = "EnterpriseProjectAdmin12#";
+            string roleName = "Admin";
+
+            // if role doesn't exist, create it
+            if (await roleManager.FindByNameAsync(roleName) == null)
+            {
+                await roleManager.CreateAsync(new IdentityRole(roleName));
+            }
+            // if username doesn't exist, create it and add it to role
+            if (await userManager.FindByNameAsync(username) == null)
+            {
+                User user = new User { UserName = username };
+                var result = await userManager.CreateAsync(user, password);
+                if (result.Succeeded)
+                {
+                    await userManager.AddToRoleAsync(user, roleName);
+                }
+            }
+        }
+    }
+}
---
+++ AI_Summarize_Quiz_Web/Models/LoginViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AI_Summarize_Quiz_Web.Models
+{
+    public class LoginViewModel
+    {
+        [Required(ErrorMessage = "Please enter a username.")]
+        [StringLength(255)]
+        public string? Username { get; set; }
+
+        [Required(ErrorMessage = "Please enter a password.")]
+        [StringLength(255)]
+        public string? Password { get; set; }
+
+        public string? ReturnUrl { get; set; }
+
+        public bool RememberMe { get; set; }
+    }
+}
