b0e4041537c976297f609453cebcf42838731b34
Gautam Singh <Gsingh3291@conestogac.on.ca>
2024-04-18 11:40:40 -0400

    Added Tests for login and register routes.

--- AutomatedTestingAPI/AutomatedTestingAPI.py
+++ AutomatedTestingAPI/AutomatedTestingAPI.py
@@ -66,3 +66,56 @@ def test_process_file_route(file_path):
     except Exception as e:
         pytest.fail(f"An error occurred: {e}")
 
+@pytest.mark.parametrize("data", [ #registers new user
+    {"Username": "newuser", "Password": "newPassword1!", "ConfirmPassword": "newPassword1!"}
+])
+def test_register_new_user_success(data):
+    REGISTER_URL = "https://localhost:7125/Account/Register"
+
+    try:
+        response = requests.post(REGISTER_URL, data=data, verify=False, allow_redirects=False)
+        assert response.status_code == 200  
+        print("Registration test passed: User registered successfully and redirected to home page.")
+    except Exception as e:
+        pytest.fail(f"Registration test failed: {e}")
+
+@pytest.mark.parametrize("data", [ #login on existing user
+    {"Username": "useruser", "Password": "@Pranav26"}
+])
+def test_login_user_success_and_redirect(data):
+    LOGIN_URL = "https://localhost:7125/Account/Login"
+
+    try:
+        with requests.Session() as session:
+            # Submit POST request with form data
+            response = session.post(LOGIN_URL, data=data, verify=False, allow_redirects=False)
+            
+            # Check for correct HTTP status 
+            assert response.status_code == 302
+            print("Login test passed: User logged in successfully and redirected to home page.")
+    except Exception as e:
+        pytest.fail(f"Login test failed: {e}")
+
+@pytest.mark.parametrize("url", ["https://localhost:7125/Account/Login"]) #makes sure login route is functional
+def test_login_page_route(url):
+    try:
+        response = requests.get(url, timeout=10, verify=False) 
+        assert response.status_code == 200  
+        print("Login Page response successful.") 
+    except requests.RequestException as e:
+        pytest.fail(f"Error occurred while making a request to the Login page: {e}")  
+        
+@pytest.mark.parametrize("registration_data", [
+    ({"Username": "newuser", "Password": "NewPassword123!", "ConfirmPassword": "NewPassword123!"})
+])
+def test_user_registration(registration_data):
+    REGISTER_URL = "https://localhost:7125/Account/Register" #checks register route
+
+    with requests.Session() as session:
+        # Send registration request
+        response = session.post(REGISTER_URL, data=registration_data, verify=False, allow_redirects=False)
+        
+        # Check for correct HTTP status for the registration request
+        assert response.status_code == 200, "Expected a successful status code on registration"
+        
+        print("Registration test passed: User registration processed successfully.")
